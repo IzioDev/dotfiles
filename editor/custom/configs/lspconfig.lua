40d10047bd3323c7587ff81d69a879f2bbcabfb5
Romain <romain.billot@harvestr.io>
2023-08-05 11:56:30 +0200

    feat: ts-tools

--- editor/custom/configs/lspconfig.lua
+++ editor/custom/configs/lspconfig.lua
@@ -2,44 +2,57 @@ local on_init = require("plugins.configs.lspconfig").on_init
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local servers = {
-  "html",
-  "cssls",
-  "rust_analyzer",
-  "tsserver",
-  "terraformls",
-  "docker_compose_language_service",
-  "pyright",
-  "prismals",
+	"html",
+	"cssls",
+	"rust_analyzer",
+	"terraformls",
+	"docker_compose_language_service",
+	"pyright",
+	"prismals",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_init = on_init,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_init = on_init,
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-local util = require "lspconfig.util"
+local util = require("lspconfig.util")
 
-lspconfig["angularls"].setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = util.root_pattern("angular.json", "project.json"),
-}
+lspconfig["angularls"].setup({
+	on_init = on_init,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	-- root_dir = util.root_pattern("angular.json", "project.json"),
+	root_dir = util.root_pattern(".git"),
+})
 
-lspconfig["yamlls"].setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    yaml = {
-      schemas = { kubernetes = "globPattern" },
-    },
-  },
-}
+-- lspconfig["tsserver"].setup {
+--   on_init = on_init,
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   -- root_dir = util.root_pattern("angular.json", "project.json"),
+--   root_dir = util.root_pattern ".git",
+-- }
 
--- require("typescript-tools").setup {}
+lspconfig["yamlls"].setup({
+	on_init = on_init,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		yaml = {
+			schemas = { kubernetes = "globPattern" },
+		},
+	},
+})
+
+require("typescript-tools").setup({
+	on_init = on_init,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = util.root_pattern(".git"),
+})
--- editor/custom/plugins.lua
+++ editor/custom/plugins.lua
@@ -1,98 +1,103 @@
-local overrides = require "custom.configs.overrides"
-local telescope_overrides = require "custom.configs.telescope.telescope"
+local overrides = require("custom.configs.overrides")
+local telescope_overrides = require("custom.configs.telescope.telescope")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- Useful Lua utilities
+	-- Override plugin definition options
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- Useful Lua utilities
 
-      -- FZF sorter for Telescope
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      { "nvim-telescope/telescope-live-grep-args.nvim" },
-    },
-    opts = telescope_overrides.telescope,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-      },
-    },
-    config = function()
-      require "custom.configs.null-ls"
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+			-- FZF sorter for Telescope
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		},
+		opts = telescope_overrides.telescope,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+			},
+		},
+		config = function()
+			require("custom.configs.null-ls")
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  -- {
-  --   "max397574/better-escape.nvim",
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("better_escape").setup()
-  --   end,
-  -- },
-  -- Copilot
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    opts = overrides.copilot,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-          require("copilot_cmp").setup()
-        end,
-      },
-    },
-    opts = {
-      sources = {
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "copilot", group_index = 2 },
-        { name = "luasnip", group_index = 2 },
-        { name = "buffer", group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path", group_index = 2 },
-      },
-    },
-  },
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- Install a plugin
+	-- {
+	--   "max397574/better-escape.nvim",
+	--   event = "InsertEnter",
+	--   config = function()
+	--     require("better_escape").setup()
+	--   end,
+	-- },
+	-- Copilot
+	{
+		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
+		opts = overrides.copilot,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{
+				"zbirenbaum/copilot-cmp",
+				config = function()
+					require("copilot_cmp").setup()
+				end,
+			},
+		},
+		opts = {
+			sources = {
+				{ name = "nvim_lsp", group_index = 2 },
+				{ name = "copilot", group_index = 2 },
+				{ name = "luasnip", group_index = 2 },
+				{ name = "buffer", group_index = 2 },
+				{ name = "nvim_lua", group_index = 2 },
+				{ name = "path", group_index = 2 },
+			},
+		},
+	},
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
